03335641c2033523a1a09b6539a1dc276bff5567
AlexE <alex.evseenko@gmail.com>
2013-08-27 23:37:02 +0300

    Update acceptance testing of a simple Java-class

---
+++ test/com/adal/test/JavaClass.java
@@ -0,0 +1,7 @@
+package com.adal.test;
+
+import java.lang.String;
+
+public class JavaClass {
+
+}
--- test/com/adal/test/JavaClassSpec.scala
+++ test/com/adal/test/JavaClassSpec.scala
@@ -4,6 +4,9 @@
 package com.adal.test
 
 import org.specs2._
+import scala.io.Source
+import javax.tools.JavaCompiler
+import javax.tools.ToolProvider
 
 /**
  * @author Alexander Evseenko
@@ -14,7 +17,7 @@ class JavaClassSpec extends SpecificationWithJUnit { def is = s2"""
   This is a specification to check generation of a Java class
   
   The JavaClass should
-    contain package declaration								$e1
+    start with package declaration							$e1
     contain imports list									$e2
   	contain keyword 'class'									$e3
   	contain name starts with capital letter 				$e4
@@ -22,18 +25,26 @@ class JavaClassSpec extends SpecificationWithJUnit { def is = s2"""
   	contain list of class fields							$e6
   	contain list of inner classes							$e7
   	contain list of class methods							$e8
-  	contain closing curly brace								$e9
+  	end with closing curly brace							$e9
+  	be compiled 											$e10
   
   """
 
-  def e1 = true
-  def e2 = true
-  def e3 = true
-  def e4 = true
-  def e5 = true
-  def e6 = true
-  def e7 = true
-  def e8 = true
-  def e9 = true
+  val javaClass = Source.fromFile("C:/Projects/git/adal/test/com/adal/test/JavaClass.java").mkString.trim
+
+  def e1 = javaClass must startWith ("package")
+  def e2 = javaClass must contain ("import")
+  def e3 = javaClass must contain ("class")
+  def e4 = javaClass must contain ("JavaClass")
+  def e5 = javaClass must contain ("{")
+  def e6 = pending
+  def e7 = pending
+  def e8 = pending
+  def e9 = javaClass must endWith ("}")
+  def e10 = {
+	val compiler = ToolProvider.getSystemJavaCompiler()
+	
+	pending
+  }
   
 }
